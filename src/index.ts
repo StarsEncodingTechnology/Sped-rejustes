d3a476173c91737cc3688b48e5b5fd72292ed3d1
Pedro Elias Costa Santos <54581042+PedroEliasCS@users.noreply.github.com>
2022-10-03 15:50:55 -0300

    voltando versão

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import logger from "./logger";
+import { SetupServer } from "./server";
+
+enum ExitStatus {
+  Failure = 1,
+  Success = 0,
+}
+
+process.on("unhandledRejection", (reason, promise) => {
+  logger.error(
+    `app saida executando a promise: ${promise} por razão de: ${reason}`
+  );
+
+  throw reason;
+});
+
+process.on("uncaughtException", (error) => {
+  logger.error(`App saida, foi capturado um erro fora do padrão: ${error}`);
+
+  process.exit(ExitStatus.Failure);
+});
+
+(async () => {
+  // iniciando
+  logger.info("Iniciando APP");
+  try {
+    const server = new SetupServer();
+    await server.init();
+    server.start();
+
+    const exitSignals: NodeJS.Signals[] = ["SIGINT", "SIGTERM", "SIGURG"];
+
+    exitSignals.map((sig) =>
+      process.on(sig, async () => {
+        try {
+          await server.close();
+          process.exit(ExitStatus.Success);
+        } catch (e) {
+          logger.error(`App exited with error: ${e}`);
+          process.exit(ExitStatus.Failure);
+        }
+      })
+    );
+  } catch (e) {
+    logger.error(`App exited with error: ${e}`);
+    process.exit(ExitStatus.Failure);
+  }
+})();
