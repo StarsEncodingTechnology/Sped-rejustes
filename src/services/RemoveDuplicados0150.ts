20dd5b90fb3591bc36ecf325f63d7733846a2cba
Pedro Elias Costa Santos <54581042+PedroEliasCS@users.noreply.github.com>
2022-10-05 17:28:12 -0300

    logica service pronta

--- src/services/RemoveDuplicados0150.ts
+++ src/services/RemoveDuplicados0150.ts
@@ -1,8 +1,8 @@
 import logger from "@src/logger";
-import { ContadorLinhas } from "@src/util/contadorLinha";
+import { ContadorLinhas, InterfaceTotalLinha } from "@src/util/contadorLinha";
 import ReadTxt from "@src/util/readtxt";
 
-interface interfaceJaAconteceu {
+interface InterfaceChaveValor {
   [key: string]: string;
 }
 
@@ -10,25 +10,26 @@ export class RemoveDuplicados0150 {
   constructor() {}
 
   public async normalizaTxt(dir: string, file: string): Promise<string[][]> {
+    // iniciando normalização de dados
     const linhasTxt = await ReadTxt.readFile(`${dir}/${file}`);
-
     return this.removeDuplicados(linhasTxt);
   }
 
   private removeDuplicados(linhasTxt: string[][]): string[][] {
-    let jaAconteceu: interfaceJaAconteceu = {};
-    let duplicados: object[] = [];
-    let duplicadosParaAconteceu: interfaceJaAconteceu = {};
+    let jaAconteceu: InterfaceChaveValor = {};
+    // variavel para saber se participantes já está cadastrado
+
+    let indexDuplicados: number[] = [];
+    //
+
+    let duplicadosParaAconteceu: InterfaceChaveValor = {};
+
 
     for (let prop in linhasTxt) {
       const element = linhasTxt[prop];
       if (element[1] == "0150") {
         if (jaAconteceu[element[5]]) {
-          duplicados.push({
-            prop,
-            codigoParticiparte: element[2],
-            codigoDestino: jaAconteceu[element[5]],
-          });
+          indexDuplicados.push(parseInt(prop));
 
           duplicadosParaAconteceu[element[2]] = jaAconteceu[element[5]];
         } else {
@@ -37,14 +38,15 @@ export class RemoveDuplicados0150 {
       }
     }
 
-    console.log({jaAconteceu, duplicados, duplicadosParaAconteceu})
-
-    return []//this.removeDuplicados(this.ajustaNotas(linhasTxt, duplicadosParaAconteceu), );
+    return this.removeLinhaDuplicada(
+      this.ajustaNotas(linhasTxt, duplicadosParaAconteceu),
+      indexDuplicados
+    );
   }
 
   private ajustaNotas(
     linhasTxt: string[][],
-    duplicadosParaAconteceu: interfaceJaAconteceu
+    duplicadosParaAconteceu: InterfaceChaveValor
   ): string[][] {
     return linhasTxt.map((element) => {
       if (element[1] == "D100" || element[1] == "C100")
@@ -55,7 +57,83 @@ export class RemoveDuplicados0150 {
     });
   }
 
-    // private removeLinhaDuplicada(linhasTxt: string[][],): string[][]{
-    //   let linhasTxtFinal: string[][] = []
-    // }
+  private removeLinhaDuplicada(
+    linhasTxt: string[][],
+    duplicados: number[]
+  ): string[][] {
+    const totalLinhas = new ContadorLinhas();
+
+    let valorFinal: string[][] = [];
+
+    for (let prop in linhasTxt) {
+      const element: string[] = linhasTxt[prop];
+      if (element[1] == "9001") break;
+
+      if (duplicados.includes(parseInt(prop)) || element[1].length != 4) {
+        continue;
+      }
+
+      totalLinhas.incrementaDecrementaLinha(element[1]);
+
+      const listaLinhasContemNumero = [
+        "0990",
+        "C990",
+        "D990",
+        "G990",
+        "H990",
+        "K990",
+        "1990",
+      ];
+      if (listaLinhasContemNumero.includes(element[1]))
+        element[2] = this.editaLinha(element[1], totalLinhas);
+
+      valorFinal.push(element);
+    }
+
+    return valorFinal.concat(this.buildBloco9(totalLinhas));
+  }
+
+  private editaLinha(linhaKey: string, totalLinhas: ContadorLinhas): string {
+    return totalLinhas.retornaTotalLinhasBloco[linhaKey[0]].toString();
+  }
+
+  public buildBloco9(totalLinhas: ContadorLinhas): string[][] {
+    const totalLinhas9900: InterfaceTotalLinha = totalLinhas.retornaTotalLinhas;
+
+    let bloco9: string[][] = [["", "9001", "0", ""]];
+    totalLinhas.incrementaDecrementaLinha("9001");
+    console.log(totalLinhas9900);
+
+    for (let prop in totalLinhas9900) {
+      bloco9.push(["", "9900", `${prop}`, `${totalLinhas9900[prop]}`, ""]);
+      totalLinhas.incrementaDecrementaLinha("9900");
+    }
+
+    // preenche final bloco 9
+    totalLinhas.incrementaDecrementaLinha("9900");
+    bloco9.push(["", "9900", "9990", "1", ""]);
+    totalLinhas.incrementaDecrementaLinha("9900");
+    bloco9.push(["", "9900", "9999", "1", ""]);
+    totalLinhas.incrementaDecrementaLinha("9900");
+    bloco9.push([
+      "",
+      "9900",
+      "9900",
+      `${totalLinhas.retornaTotalLinhas["9900"]}`,
+      "",
+    ]);
+
+    totalLinhas.incrementaDecrementaLinha("9990");
+    totalLinhas.incrementaDecrementaLinha("9999");
+    bloco9.push([
+      "",
+      "9990",
+      `${totalLinhas.retornaTotalLinhasBloco["9"]}`,
+      "",
+    ]);
+    bloco9.push(["", "9999", `${totalLinhas.retornaNumero.toString()}`, ""]);
+
+    // fim preenche bloco 9
+    return bloco9;
+  }
 }
--- src/util/contadorLinha.ts
+++ src/util/contadorLinha.ts
@@ -4,6 +4,8 @@ export interface InterfaceTotalLinha {
 
 export class ContadorLinhas {
   private totalLinhas: InterfaceTotalLinha = {};
+  private totalLinhasBloco: InterfaceTotalLinha = {};
+  private numeroTotalLinhas: number = 0;
   // declara global
   constructor() {}
 
@@ -16,16 +18,26 @@ export class ContadorLinhas {
   }
 
   public incrementaDecrementaLinha(keyLinha: string, valor: number = 1): void {
-    if (!this.totalLinhas[keyLinha]) {
-      this.totalLinhas[keyLinha] = 0;
-      console.log(keyLinha)
-    }
+    if (!this.totalLinhas[keyLinha]) this.totalLinhas[keyLinha] = 0;
+    if (!this.totalLinhasBloco[keyLinha[0]])
+      this.totalLinhasBloco[keyLinha[0]] = 0;
 
-    // console.log(keyLinha + " // " + this.totalLinhas[keyLinha]);
+    this.totalLinhasBloco[keyLinha[0]] =
+      this.totalLinhasBloco[keyLinha[0]] + valor;
+
     this.totalLinhas[keyLinha] = this.totalLinhas[keyLinha] + valor;
+
+    this.numeroTotalLinhas = this.numeroTotalLinhas + valor;
   }
 
   get retornaTotalLinhas(): InterfaceTotalLinha {
     return this.totalLinhas;
+  }
+
+  get retornaTotalLinhasBloco(): InterfaceTotalLinha {
+    return this.totalLinhasBloco;
+  }
+  get retornaNumero(): number {
+    return this.numeroTotalLinhas;
   }
 }
