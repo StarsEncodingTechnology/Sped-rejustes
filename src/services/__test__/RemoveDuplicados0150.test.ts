a9831daafc74c9702a5c2ce12e9b05f4743a0ee5
Pedro Elias Costa Santos <54581042+PedroEliasCS@users.noreply.github.com>
2022-10-09 17:31:00 -0300

    resolvendo erro de teste

--- src/services/__test__/RemoveDuplicados0150.test.ts
+++ src/services/__test__/RemoveDuplicados0150.test.ts
@@ -1,14 +1,18 @@
-import { join } from "path";
+import path, { join } from "path";
 import { RemoveDuplicados0150 } from "../RemoveDuplicados0150";
 import dadosSemduplicados from "@test/fixtures/dadosSemDuplicados.json";
 
 describe("RemoveDuplicados0150", () => {
-  const dirTxt = join(__dirname, "../../../test/fixtures");
-  it("deve retornar o txt normalizado CORRETO", async () => {
+  const resolvePath = path.resolve("test");
+  it.only("deve retornar o txt normalizado CORRETO", async () => {
+    console.log(resolvePath);
     const nameFile = "post_spedComDuplicados.txt";
     const removeDuplicados0150 = new RemoveDuplicados0150();
 
-    const response = await removeDuplicados0150.normalizaTxt(dirTxt, nameFile);
+    const response = await removeDuplicados0150.normalizaTxt(
+      resolvePath + "/fixtures/",
+      nameFile
+    );
 
     const respostaCorreta = Array.from(dadosSemduplicados);
     expect(response).toEqual(respostaCorreta);
@@ -19,7 +23,7 @@ describe("RemoveDuplicados0150", () => {
     const removeDuplicados0150 = new RemoveDuplicados0150();
 
     await expect(
-      removeDuplicados0150.normalizaTxt(dirTxt, nameFile)
+      removeDuplicados0150.normalizaTxt((resolvePath + "/fixtures/"), nameFile)
     ).rejects.toThrow(
       "Erro quando o arquivo não é valido: Linhas faltantes - 9999,D990,E990,G990,H990,K990,1990,9990"
     );
@@ -30,7 +34,7 @@ describe("RemoveDuplicados0150", () => {
     const removeDuplicados0150 = new RemoveDuplicados0150();
 
     await expect(
-      removeDuplicados0150.normalizaTxt(dirTxt, nameFile)
+      removeDuplicados0150.normalizaTxt((resolvePath + "/fixtures/"), nameFile)
     ).rejects.toThrow(
       "Erro quando o arquivo não é valido: Arquivo de formato invalido"
     );
